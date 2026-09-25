72f60666b58dcdcc227b6eeb44849412948b1c20
muneeb afridi <muneebafridi2525@gmail.com>
2026-09-25 21:40:07 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-base: '/PDF-Website-for-practice/'
 
 export default defineConfig({
-  base: './',
+  base: '/PDF-Website-for-practice/',
   plugins: [react()],
 })
